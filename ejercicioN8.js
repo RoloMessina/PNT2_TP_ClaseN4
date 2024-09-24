19adbddb6651a7cd07b839142f2f37db1b089373
RoloMessina <113044165+RoloMessina@users.noreply.github.com>
2024-09-24 17:49:21 -0300

    Se encripto la API_key

--- ejercicioN8.js
+++ ejercicioN8.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const axios = require('axios');
 
 async function obtenerClima(ciudad) {
-    const apiKey = 'b776a52b7f1c0e5f7fff73478409c0c7';
+    const apiKey = process.env.OPENWEATHER_API_KEY; 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric&lang=es`;
 
     try {
@@ -15,5 +16,4 @@ async function obtenerClima(ciudad) {
     }
 }
 
-// Llama a la función con la ciudad deseada
 obtenerClima('Buenos Aires');
